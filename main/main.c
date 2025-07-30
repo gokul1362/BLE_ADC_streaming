6f275e4c9f169c64d0749eec7b745618cef6e3e4
gokul1362 <gokulks1362@gmail.com>
2025-07-30 13:09:12 +0200

    Notification enabled for sampler

--- main/main.c
+++ main/main.c
@@ -1,33 +1,40 @@
 #include <stdio.h>
 #include <string.h>
 #include "freertos/FreeRTOS.h"
-#include "nvs_flash.h"
+#include "freertos/task.h"
 #include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_system.h"
 #include "esp_bt.h"
+#include "esp_bt_main.h"
 #include "esp_gap_ble_api.h"
 #include "esp_gatts_api.h"
-#include "esp_bt_main.h"
 #include "esp_gatt_common_api.h"
+#include "driver/adc.h"
 
-#define TAG "BLE_SAMPLER"
+#define TAG "SAMPLER"
 
-#define SERVICE_UUID        0x1234
-#define CHAR_UUID           0x5678
-#define GATTS_NUM_HANDLE    4
-#define DEVICE_NAME         "BLE_Sampler"
+#define SAMPLER_SERVICE_UUID   0x00FF
+#define STREAM_CHAR_UUID       0xFF01
+#define GATTS_NUM_HANDLE       4
+#define DEVICE_NAME            "BLE_Sampler"
+
+#define SAMPLE_COUNT 10
+#define ADC_CHANNEL  ADC1_CHANNEL_0
 
 static uint16_t service_handle;
 static uint16_t char_handle;
-
-static uint8_t char_value[1] = { 2 };
+static esp_gatt_if_t global_gatts_if;
+static uint16_t conn_id = 0;
+static bool is_connected = false;
 
 static esp_ble_adv_params_t adv_params = {
-    .adv_int_min        = 0x20,
-    .adv_int_max        = 0x40,
-    .adv_type           = ADV_TYPE_IND,
-    .own_addr_type      = BLE_ADDR_TYPE_PUBLIC,
-    .channel_map        = ADV_CHNL_ALL,
-    .adv_filter_policy  = ADV_FILTER_ALLOW_SCAN_ANY_CON_ANY,
+    .adv_int_min = 0x20,
+    .adv_int_max = 0x40,
+    .adv_type = ADV_TYPE_IND,
+    .own_addr_type = BLE_ADDR_TYPE_PUBLIC,
+    .channel_map = ADV_CHNL_ALL,
+    .adv_filter_policy = ADV_FILTER_ALLOW_SCAN_ANY_CON_ANY,
 };
 
 static esp_ble_adv_data_t adv_data = {
@@ -36,21 +43,41 @@ static esp_ble_adv_data_t adv_data = {
     .flag = (ESP_BLE_ADV_FLAG_GEN_DISC | ESP_BLE_ADV_FLAG_BREDR_NOT_SPT),
 };
 
+void sample_adc_bundle_and_notify(uint16_t conn_id, uint16_t char_handle, esp_gatt_if_t gatts_if)
+{
+    static bool adc_initialized = false;
+    uint16_t samples[SAMPLE_COUNT];
+    uint8_t payload[SAMPLE_COUNT * 2];
+
+    if (!adc_initialized) {
+        adc1_config_width(ADC_WIDTH_BIT_12);
+        adc1_config_channel_atten(ADC_CHANNEL, ADC_ATTEN_DB_11);
+        adc_initialized = true;
+    }
+
+    for (int i = 0; i < SAMPLE_COUNT; i++) {
+        samples[i] = adc1_get_raw(ADC_CHANNEL);
+        payload[2 * i] = samples[i] & 0xFF;
+        payload[2 * i + 1] = (samples[i] >> 8) & 0xFF;
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
+
+    esp_ble_gatts_send_indicate(gatts_if, conn_id, char_handle, sizeof(payload), payload, false);
+    ESP_LOGI(TAG, "Sent ADC sample bundle via notify");
+}
+
+void adc_stream_task(void *param)
+{
+    while (is_connected) {
+        sample_adc_bundle_and_notify(conn_id, char_handle, global_gatts_if);
+    }
+    vTaskDelete(NULL);
+}
+
 static void gap_event_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param)
 {
-    switch (event) {
-    case ESP_GAP_BLE_ADV_DATA_SET_COMPLETE_EVT:
-        ESP_LOGI(TAG, "Adv data set, starting advertising...");
+    if (event == ESP_GAP_BLE_ADV_DATA_SET_COMPLETE_EVT) {
         esp_ble_gap_start_advertising(&adv_params);
-        break;
-    case ESP_GAP_BLE_ADV_START_COMPLETE_EVT:
-        ESP_LOGI(TAG, "Adv start %s", param->adv_start_cmpl.status == ESP_BT_STATUS_SUCCESS ? "success" : "failed");
-        break;
-    case ESP_GAP_BLE_ADV_STOP_COMPLETE_EVT:
-        ESP_LOGI(TAG, "Adv stopped");
-        break;
-    default:
-        break;
     }
 }
 
@@ -59,33 +86,27 @@ static void gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_
 {
     switch (event) {
     case ESP_GATTS_REG_EVT: {
-        ESP_LOGI(TAG, "GATT registered, app_id=%d", param->reg.app_id);
-
         esp_ble_gap_set_device_name(DEVICE_NAME);
         esp_ble_gap_config_adv_data(&adv_data);
 
         esp_gatt_srvc_id_t service_id = {
             .is_primary = true,
-            .id.inst_id = 0x00,
+            .id.inst_id = 0,
             .id.uuid.len = ESP_UUID_LEN_16,
-            .id.uuid.uuid.uuid16 = SERVICE_UUID,
+            .id.uuid.uuid.uuid16 = SAMPLER_SERVICE_UUID,
         };
-
         esp_ble_gatts_create_service(gatts_if, &service_id, GATTS_NUM_HANDLE);
         break;
     }
     case ESP_GATTS_CREATE_EVT: {
         service_handle = param->create.service_handle;
-        ESP_LOGI(TAG, "Service created, handle=%d", service_handle);
-
         esp_ble_gatts_start_service(service_handle);
 
         esp_bt_uuid_t char_uuid = {
             .len = ESP_UUID_LEN_16,
-            .uuid.uuid16 = CHAR_UUID,
+            .uuid.uuid16 = STREAM_CHAR_UUID,
         };
-
-        esp_gatt_char_prop_t prop = ESP_GATT_CHAR_PROP_BIT_READ;
+        esp_gatt_char_prop_t prop = ESP_GATT_CHAR_PROP_BIT_NOTIFY;
 
         esp_ble_gatts_add_char(service_handle, &char_uuid,
                                ESP_GATT_PERM_READ,
@@ -94,26 +115,31 @@ static void gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_
     }
     case ESP_GATTS_ADD_CHAR_EVT: {
         char_handle = param->add_char.attr_handle;
-        ESP_LOGI(TAG, "Characteristic added, handle=%d", char_handle);
+            esp_bt_uuid_t descr_uuid = {
+        .len = ESP_UUID_LEN_16,
+        .uuid.uuid16 = ESP_GATT_UUID_CHAR_CLIENT_CONFIG,
+       };
+
+        esp_gatt_perm_t perm = ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE;
+
+        esp_ble_gatts_add_char_descr(service_handle, &descr_uuid, perm, NULL, NULL);
         break;
     }
-    case ESP_GATTS_READ_EVT: {
-        ESP_LOGI(TAG, "Read requested");
-        esp_gatt_rsp_t rsp = {0};
-        rsp.attr_value.handle = param->read.handle;
-        rsp.attr_value.len = sizeof(char_value);
-        rsp.attr_value.value[0] = char_value[0];
-        esp_ble_gatts_send_response(gatts_if, param->read.conn_id, param->read.trans_id,
-                                    ESP_GATT_OK, &rsp);
+    case ESP_GATTS_CONNECT_EVT: {
+        is_connected = true;
+        conn_id = param->connect.conn_id;
+        global_gatts_if = gatts_if;
+
+        ESP_LOGI(TAG, "Client connected. Starting sampling task.");
+        xTaskCreate(adc_stream_task, "adc_stream_task", 4096, NULL, 5, NULL);
         break;
     }
-    case ESP_GATTS_CONNECT_EVT:
-        ESP_LOGI(TAG, "Client connected");
-        break;
-    case ESP_GATTS_DISCONNECT_EVT:
-        ESP_LOGI(TAG, "Client disconnected, restarting adv");
+    case ESP_GATTS_DISCONNECT_EVT: {
+        is_connected = false;
         esp_ble_gap_start_advertising(&adv_params);
+        ESP_LOGI(TAG, "Client disconnected, restarted advertising.");
         break;
+    }
     default:
         break;
     }
